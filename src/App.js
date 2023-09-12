25a4423c90cbaefdba233f411bd061ceee5e2854
OsamaMalik651 <osama.malik013@gmai.com>
2023-09-12 10:48:36 -0600

    added json server for backend. implemented axios to make api calls for crud operations.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
+import axios from 'axios';
 
 const App = () => {
     const [books, setBooks] = useState([]);
 
-    const createBook = (title) => {
-        setBooks([...books, { id: Math.round(Math.random() * 9999), title }])
+    const fetchbooks = async () => {
+        const response = await axios.get('http://localhost:3001/books',);
+        setBooks(response.data);
+    }
+    useEffect(() => {
+        fetchbooks();
+    }, []);
+
+    const createBook = async (title) => {
+        // setBooks([...books, { id: Math.round(Math.random() * 9999), title }])
+        const response = await axios.post('http://localhost:3001/books', {
+            title
+        });
+        const updatedBooks = [...books, response.data]
+        setBooks(updatedBooks)
     };
 
-    const deleteBookById = (id) => {
+    const deleteBookById = async (id) => {
+        await axios.delete(`http://localhost:3001/books/${id}`);
         const updatedBooks = books.filter((book) => {
             return book.id !== id
         });
         setBooks(updatedBooks)
     }
-    const editBookById = (id, title) => {
+    const editBookById = async (id, title) => {
+
+        // setBooks(updatedBooks)
+        const response = await axios.put(`http://localhost:3001/books/${id}`, {
+            title: title
+        });
         const updatedBooks = books.map((book) => {
             if (book.id === id) {
-                return { ...book, title: title }
+                return { ...book, ...response.data }
             }
             return book
         });
